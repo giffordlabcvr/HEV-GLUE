d39c8d228e5dcbfb15e093e659d2a2c4e6ac15b4
Josh Singer <josh.singer@glasgow.ac.uk>
2017-03-16 16:57:22 +0000

    added cookie disclaimer and analytics.

--- WebContent/hevApp.js
+++ WebContent/hevApp.js
@@ -1,3 +1,20 @@
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https:www.google-analytics.com/analytics.js','ga');
+	
+	  console.log("document.location.hostname", document.location.hostname);
+	  var trackingID;
+	  if(document.location.hostname.indexOf("hev.glue.cvr.ac.uk") >= 0) {
+		  // HEV-GLUE production analytics account
+		  trackingID = 'UA-93776838-1';
+		  ga('create', trackingID, 'auto');
+	  } else {
+		  // sandbox analytics account
+		  trackingID = 'UA-93752139-1';
+		  ga('create', trackingID, 'none');
+	  }
+
 var hevApp = angular.module('hevApp', [
     'ngRoute',
     'analysisTool', 
@@ -5,7 +22,10 @@ var hevApp = angular.module('hevApp', [
     'home',
     'glueWS',
     'glueWebToolConfig',
-    'treeControl'
+    'treeControl',
+    'angulartics',
+    'angulartics.google.analytics',
+    'angular-cookie-law'
   ]);
 
 console.log("after hevApp module definition");
